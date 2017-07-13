932f06906d853494304b2cc514fd5d46dc63b1c2
Francisco Carlos Moraes Junior <moraes.junior6@gmail.com>
2017-07-12 23:23:34 -0300

    Adicionando Controllers e Diretivas a aplicação

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,36 +1,12 @@
-angular.module('projeto')
-    .controller('FotoController', function($scope, $routeParams, $location, FotoService) {
+angular.module('module-main').controller('FotoController',function($scope,$http){
 
-        $scope.foto = {};
-        $scope.mensagem = '';
-    
-        if($routeParams.fotoId) {
-            FotoService.get({fotoId: $routeParams.fotoId}, function(retorno) {
-                 $scope.foto = retorno;
-            });
-        }
+	$scope.fotos = [];
 
-        $scope.submeter = function() {
-           if($scope.formulario.$valid) {
-                if(!$routeParams.fotoId) {
-                    
-                    FotoService.save($scope.foto, function(_id) {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Salvo com sucesso';
-                    }, function(status) {
-                        console.log(status);
-                        $scope.mensagem = 'Não foi possível salvar';
-                    });
-
-                } else {
-                    FotoService.update({fotoId : $scope.foto._id}, $scope.foto, function(_id) {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Alterado com sucesso';                        
-                    }, function(status) {
-                        console.log(status);
-                        $scope.mensagem = 'Não foi possível alterar';
-                    });                    
-                }
-           } 
-        };
-    });
+	$http.get('/v1/fotos')
+	.success(function(fotos){
+		$scope.fotos = fotos;
+	})
+	.catch(function(error){
+		console.log(error)
+	});	
+});
