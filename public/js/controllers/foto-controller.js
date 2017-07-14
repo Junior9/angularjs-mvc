3ba54bba8267770acca38edae538add4326a6b8b
Francisco Carlos Moraes Junior <moraes.junior6@gmail.com>
2017-07-14 19:17:14 -0300

    Configuração de rotas e animações

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -2,6 +2,8 @@ angular.module('module-main').controller('FotoController',function($scope,$http)
 
 	$scope.fotos = [];
 
+	$scope.filtro = '';
+
 	$http.get('/v1/fotos')
 	.success(function(fotos){
 		$scope.fotos = fotos;
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,25 @@
-angular.module('module-main',['minhasDiretivas']);
+angular.module('module-main', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+    .config(function($routeProvider,$locationProvider) {
+
+
+    	/*
+			Desabelita o uso do # nas url mais o backEnd tem que estar preparado e o Browser suportar	
+			$locationProvider.html5Mode(true);
+    	*/
+    	
+        $routeProvider.when('/fotos', {
+            templateUrl: 'partials/principal.html',
+            controller: 'FotoController'
+        });
+
+        $routeProvider.when('/fotos/new', {
+            templateUrl: 'partials/fotos.html',
+            controller: ''
+        });
+
+
+        $routeProvider.otherwise({redirectTo:'/fotos'});
+        
+   	});
+
+
