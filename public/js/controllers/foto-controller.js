c13f05dd599bd8076bb5af7a113a0f211393b174
Francisco Carlos Moraes Junior <moraes.junior6@gmail.com>
2017-07-16 13:03:20 -0300

    Validaçao do formulario de novas fotos

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,8 +1,23 @@
-angular.module('module-main').controller('FotoController',function($scope){
+angular.module('module-main').controller('FotoController',function($scope,$http){
 
 	$scope.foto = {}
+	$scope.mensagem = {}
+
 	$scope.subMeter = function(){
-		console.log($scope.foto);
+
+		if ($scope.formulario.$valid){
+
+			$http.post('v1/fotos',$scope.foto)
+			.success(function (){			
+				$scope.foto = {};
+				$scope.mensagem = 'Foto casdastrada com sucesso';
+				console.log("foto cadastrada");
+			})
+			.error(function (erro){
+				$scope.mensagem = 'Erro ao cadastrar foto';
+				console.log(erro)
+			});
+		}
 	};
 
 });
